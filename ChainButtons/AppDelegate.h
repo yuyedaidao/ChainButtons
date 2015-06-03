a261e325f39a037bd25069aba7134dc795eedd07
yuyedaidao <wyqpadding@gmail.com>
2015-06-03 16:32:15 +0800

    Initial Commit

---
+++ ChainButtons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChainButtons
+//
+//  Created by Wang on 15/6/3.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChainButtons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChainButtons
+//
+//  Created by Wang on 15/6/3.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
